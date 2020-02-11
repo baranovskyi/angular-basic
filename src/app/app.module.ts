78b776be344f8fa620192698d3cdc238de6d10d5
Baranovskyi Oleksandr <obaranovskyi@exadel.net>
2020-02-11 15:35:43 +0200

    angular practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { TopBarComponent } from "./top-bar/top-bar.component";
+import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
+import { ProductDetailsComponent } from "./product-details/product-details.component";
+import { CartComponent } from "./cart/cart.component";
+import { HttpClientModule } from "@angular/common/http";
+import { ShippingComponent } from "./shipping/shipping.component";
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: "", component: ProductListComponent },
+      { path: "products/:productId", component: ProductDetailsComponent },
+      { path: "cart", component: CartComponent },
+      { path: "shipping", component: ShippingComponent }
+    ])
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
